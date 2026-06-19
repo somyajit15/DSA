06a61356e1666479e37c4c50122afd0508bcda57
somyajit15 <somyajit22@gmail.com>
2026-06-19 22:40:36 +0530

    Add files via upload

---
+++ Second-largest-element/Second-largest-element.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } 
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    if (secondLargest == INT_MIN) {
+        cout << "No second largest element exists";
+    } else {
+        cout << secondLargest;
+    }
+
+    return 0;
+}
