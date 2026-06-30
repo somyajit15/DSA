aef0f359c78e8564789838af2f0c8f46fd7701ab
somyajit15 <somyajit22@gmail.com>
2026-06-30 23:45:29 +0530

    Add files via upload

---
+++ Frequency-of-element/Frequency-element.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int x;
+    cin >> x;
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
