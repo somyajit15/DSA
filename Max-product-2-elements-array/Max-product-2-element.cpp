ed5aea4be38249671b62d8c1c753bc057b40feaa
somyajit15 <somyajit22@gmail.com>
2026-07-14 22:27:52 +0530

    Add files via upload

---
+++ Max-product-2-elements-array/Max-product-2-element.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    cout << largest * secondLargest;
+
+    return 0;
+}
